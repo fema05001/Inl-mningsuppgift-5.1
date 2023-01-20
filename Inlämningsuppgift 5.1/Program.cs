013490592f35379542221749b2ee72dd60e0f56a
Pingvu <nhlpool27@gmail.com>
2023-01-20 13:45:35 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_5_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[5];
+            a[0] = ("Hej och välkommen");
+            a[1] = ("Till mitt program");
+            a[2] = ("Där meningar skrivs ut");
+            a[3] = ("En efter den andra");
+            a[4] = ("Tills programet är klart.");
+            
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine(a[i]);
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
